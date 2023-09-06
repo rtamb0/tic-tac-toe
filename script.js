116b13040e0924e75281d0ea2e675db8eafc75bd
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-09-07 00:08:36 +0700

    Change the value of the cell when clicked, both in HTML and array

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
+// Create a module that controls the game (click event, etc)
+
+const displayController = (() => {
+    const changeValue = function(arr, index) {
+        this.innerHTML = 'X';
+        arr[index] = 'X';
+    };
+    return {changeValue};
+})();
+
 // Store the gameboard as an array inside of gameboard object as a module
 
 const gameBoard = (() => {
-    const gameArr = [['X', 'O', 'X'], ['X', 'O', 'O'], ['X', 'O', 'O']];
+    const gameArr = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
     const containerHTML = document.querySelector('.gameboard');
     const render = () => {
         gameArr.forEach((row) => {
             const rowHTML = document.createElement('div');
             rowHTML.classList.add('row-game');
-            row.forEach((cell) => {
+            row.forEach((cell, i) => {
                 const cellHTML = document.createElement('span');
                 cellHTML.classList.add('cell-game');
                 cellHTML.innerHTML = cell;
+                cellHTML.addEventListener('click', displayController.changeValue.bind(cellHTML, row, i));
                 rowHTML.appendChild(cellHTML);
             });
             containerHTML.appendChild(rowHTML);
         });
     };
-    return {render};
+    return {render, gameArr};
 })();
 
 gameBoard.render()
@@ -27,5 +38,3 @@ const player = (name) => {
     const getName = () => name;
     return {getName};
 };
-
-// Create a module that controls the game (click event, etc)
