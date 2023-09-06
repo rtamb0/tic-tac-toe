430c5d82fa124ff5de9d2c7ef3b8abc7f61e76e4
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-09-06 22:04:41 +0700

    Create a gameBoard module that renders the gameboard array into the page

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 // Store the gameboard as an array inside of gameboard object as a module
 
 const gameBoard = (() => {
-    const gameArr = [];
-    return {gameArr};
+    const gameArr = [['X', 'O', 'X'], ['X', 'O', 'O'], ['X', 'O', 'O']];
+    const containerHTML = document.querySelector('.gameboard');
+    const render = () => {
+        gameArr.forEach((row) => {
+            const rowHTML = document.createElement('div');
+            rowHTML.classList.add('row-game');
+            row.forEach((cell) => {
+                const cellHTML = document.createElement('span');
+                cellHTML.classList.add('cell-game');
+                cellHTML.innerHTML = cell;
+                rowHTML.appendChild(cellHTML);
+            });
+            containerHTML.appendChild(rowHTML);
+        });
+    };
+    return {render};
 })();
+
+gameBoard.render()
 
 // Create a factory function that creates the player
 
 const player = (name) => {
-    const name = name;
-    return {name};
+    const getName = () => name;
+    return {getName};
 };
 
 // Create a module that controls the game (click event, etc)
