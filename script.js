8389f7b65d2bb1f674c2725780b28910e4eaa3fc
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-09-06 18:31:21 +0700

    Create a module of gameBoard

---
+++ script.js
@@ -0,0 +1,10 @@
+// Store the gameboard as an array inside of gameboard object as a module
+
+const gameBoard = (() => {
+    const gameArr = [];
+    return {gameArr};
+})();
+
+// Create a factory function that creates the player
+
+// Create a module that controls the game (click event, etc)
